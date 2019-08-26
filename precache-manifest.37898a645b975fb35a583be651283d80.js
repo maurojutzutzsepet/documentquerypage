e1a41f2b5291c59338d52252ec597bd9440f1e36
Mauro Jutzutz Sepet <mauro.jutzutz@gmail.com>
2019-08-26 03:25:47 -0600

    Updates

---
+++ precache-manifest.37898a645b975fb35a583be651283d80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eede8aeff15f0151eaeb06d4748c59c",
+    "url": "/documentquerypage/index.html"
+  },
+  {
+    "revision": "6ee03eb57aa82c42ef09",
+    "url": "/documentquerypage/static/css/main.1e0d3af7.chunk.css"
+  },
+  {
+    "revision": "6a8bc9e51c4c32c74783",
+    "url": "/documentquerypage/static/js/2.0c8e7434.chunk.js"
+  },
+  {
+    "revision": "6ee03eb57aa82c42ef09",
+    "url": "/documentquerypage/static/js/main.538432ef.chunk.js"
+  },
+  {
+    "revision": "9582d933d26968d02579",
+    "url": "/documentquerypage/static/js/runtime~main.14f55bba.js"
+  }
+]);
